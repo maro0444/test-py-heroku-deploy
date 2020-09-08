bcf806e27cd1e69686c84b57d92644e19ffe5a3f
Matt R <roush.matt@gmail.com>
2020-09-08 16:47:18 -0600

    change msg to hello friend

--- hello/views.py
+++ hello/views.py
@@ -7,7 +7,7 @@ from .models import Greeting
 # Create your views here.
 def index(request):
     times = int(os.environ.get('TIMES',3))
-    return HttpResponse('Hello! ' * times)
+    return HttpResponse('Hello friend! ' * times)
 
 
 def db(request):
